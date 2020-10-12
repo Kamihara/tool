11b9853c07f12d5b8513999b356bca1a54e69de7
Kamihara <yoshihara72@gmail.com>
2020-10-12 22:43:08 +0900

    dir整理

---
+++ bk/cp_tabelog.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp /Users/YoshitoKamihara/development/python_script/tabelog/tabelog_data_getter.py /Users/YoshitoKamihara/Documents/aiit/PBL/dev/gourmet/kamihara/tabelog_data_getter.py
+
